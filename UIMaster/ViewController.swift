5f515dfe5ca30f883de0f9e4d3fb79d031aa1377
YuGeonHui <ghyu_0906@naver.com>
2022-09-05 20:57:15 +0900

    무한 스크롤 애니메이션 구현완료

--- UIMaster/ViewController.swift
+++ UIMaster/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let rollingStringData: [String] = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
+    private var selectedIndex = 0
+    
+    @IBOutlet weak var rollingScrollView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rollingScrollView.delegate = self
+        rollingScrollView.dataSource = self
+        rollingScrollView.isUserInteractionEnabled = false
+        
+        self.trigger()
     }
-
-
+    
+    private func trigger() {
+        
+        let _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(scrollIndex), userInfo: nil, repeats: true)
+    }
+    
+    @objc func scrollIndex() {
+        
+        let row: Int = selectedIndex
+        
+        rollingScrollView.selectRow(row, inComponent: 0, animated: true)
+        selectedIndex = selectedIndex + 1
+    }
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10000
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let oneWeeky = self.rollingStringData
+        return oneWeeky[row % oneWeeky.count]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        rollingScrollView.reloadComponent(component)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+
+        let oneWeek = rollingStringData
+        let textFiled = UITextField()
+        
+        pickerView.subviews.forEach { $0.backgroundColor = .clear }
+        
+        if oneWeek.count == 0 {
+
+            return textFiled
+
+        } else {
+            
+            textFiled.text = oneWeek[row % rollingStringData.count]
+            textFiled.textAlignment = .center
+            return textFiled
+        }
+    }
+    
+}
