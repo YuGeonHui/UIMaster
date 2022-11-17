327bbe4dc9f8eefb72101c6eb044ce151d24905e
YuGeonHui <ghyu_0906@naver.com>
2022-11-17 11:10:34 +0900

    WKYoutubeView 파일 생성

---
+++ UIMaster/YoutubeView/WKYoutubeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WKYoutubeViewController.swift
+//  UIMaster
+//
+//  Created by geonhui Yu on 2022/11/17.
+//
+
+import UIKit
+
+class WKYoutubeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
